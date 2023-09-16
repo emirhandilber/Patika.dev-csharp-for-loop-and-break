1c1cde9d01616f02d573ec6d4c8137af15d4a297
Emirhan Dilber <edilberfb@gmail.com>
2023-09-16 21:32:25 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace donguler_for_loop{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Sayıyı giriniz :");
+            int number1 = int.Parse(Console.ReadLine());
+            for (int i = 1; i < number1 ; i++)
+            {
+                if(i%2 != 0)
+                    Console.WriteLine(i);
+            }
+            int tektoplam = 0;
+            int çifttoplam =0;
+            for (int i = 0; i <= 1000; i++)
+            {
+                if(i % 2 == 1)
+                    tektoplam +=i;
+                else
+                    çifttoplam +=i;
+            }
+            Console.WriteLine("Tek toplam = "+ tektoplam);
+            Console.WriteLine("Çift toplam = "+ çifttoplam); 
+        }
+    }
+}
